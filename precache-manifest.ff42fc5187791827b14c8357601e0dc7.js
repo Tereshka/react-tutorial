87a458b60c27d82f79dc1f776f0872cb3a37d273
Tereshka <tereshka07@gmail.com>
2019-07-25 13:27:38 +0900

    Updates

---
+++ precache-manifest.ff42fc5187791827b14c8357601e0dc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90727148283308b8033a24b8f646a62",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "c822c436253c9125e5fc",
+    "url": "/react-tutorial/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "c28a6cedc3e3758a2cfc",
+    "url": "/react-tutorial/static/js/2.02fbbebc.chunk.js"
+  },
+  {
+    "revision": "c822c436253c9125e5fc",
+    "url": "/react-tutorial/static/js/main.1a865fd5.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
